2a03a91815271a1f9451af8d3a08ea397f3faf65
Pamudi593 <pabasaranishanthi@gmail.com>
2026-09-02 15:50:39 +0530

    new

--- test.c
+++ test.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("helloworld");
-    printf("welcome ")
+    printf("helloworld"/n);
+    printf("welcome "/n);
+    printf("github session"/n);
 
     return 0;
 }
